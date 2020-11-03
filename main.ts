fb3639f4cdf4af1cce65e9cf3f686b60351f5fd4
Mikayla-Barthelette <71400623+Mikayla-Barthelette@users.noreply.github.com>
2020-11-03 13:35:47 -0500

    Added main code

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(robotbit.Servos.S1, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 3; index++) {
+        robotbit.Servo(robotbit.Servos.S1, 0)
+        basic.pause(1000)
+        robotbit.Servo(robotbit.Servos.S1, 90)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(robotbit.Servos.S1, 90)
+})
+// Created by: Mikayla Barthelette
+// Created on: Nov. 2020
+// 
+// This program allows you to turn a servo when you press a button.
 basic.forever(function () {
 	
 })
